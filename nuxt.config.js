b06877cd89affb1bf9b6c43e91777aac2f259e9a
Sachinda Liyanaarachchi <sachinda.yl@gmail.com>
2021-03-26 11:21:17 +0530

    favicon change

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,6 +16,24 @@ export default {
       { hid: 'description', name: 'description', content: '' },
     ],
     link: [
+      {
+        rel: "apple-touch-icon",
+        sizes: "180x180",
+        href: "/apple-touch-icon.png"
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        href: "/favicon-32x32.png"
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        href: "/favicon-16x16.png"
+      },
+      { rel: "manifest", href: "/site.webmanifest" },
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       {
         rel: 'stylesheet',
@@ -43,7 +61,7 @@ export default {
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: ['@nuxtjs/axios', '@nuxtjs/firebase', ['vue-scrollto/nuxt', { duration: 500, offset: -100 }], 'nuxt-compress'],
+  modules: ['@nuxtjs/axios', '@nuxtjs/firebase', ['vue-scrollto/nuxt', { duration: 300, offset: -90 }], 'nuxt-compress'],
 
   firebase: {
     config: {
